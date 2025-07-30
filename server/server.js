a4f73589c629a6e40f8132a0dfd397859fef8695
sumit <sumitkushwahatg@gmail.com>
2025-07-30 15:14:16 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -434,7 +434,8 @@ app.post('/api/orders', authMiddleware, async (req, res) => {
 
 
 
-app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-      
